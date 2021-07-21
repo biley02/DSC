6d5a4623bd936e457bfc173faa70e7bc1535a841
biley02 <57981298+biley02@users.noreply.github.com>
2021-07-21 23:30:25 +0530

    make it better

--- src/account/nodemailer.js
+++ src/account/nodemailer.js
@@ -1,11 +1,13 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 const cryptoRandomString = require("crypto-random-string");
-const { JsonWebTokenError } = require("jsonwebtoken");
+const {
+    JsonWebTokenError
+} = require("jsonwebtoken");
 const jwt = require("jsonwebtoken");
 
 function sendBody(subject, body, content, link, button) {
-  return `<!DOCTYPE html>
+    return `<!DOCTYPE html>
     <html lang="en">
       <head>
         <title></title>
@@ -427,280 +429,279 @@ function sendBody(subject, body, content, link, button) {
 
 //for sending to user
 const contact = (data) => {
-  var transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.NODEMAILER_EMAIL, //email id
-      pass: process.env.NODEMAILER_PASSWORD, //my gmail password
-    },
-  });
+    var transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.NODEMAILER_EMAIL, //email id
+            pass: process.env.NODEMAILER_PASSWORD, //my gmail password
+        },
+    });
 
-  var mailOptions = {
-    from: process.env.NODEMAILER_EMAIL,
-    to: `${data.email}`,
-    subject: `DSC - ${data.subject}`,
-    html: sendBody(
-      data.subject,
-      `<p>Hello ${data.name},<br>"Your Issue" - ${data.message} <br /> has been recieved and we will solve this issue soon!!</p>`,
-      "Meanwhile you can explore our website by clicking on the button below",
-      "https://dscnits.tech",
-      "Click Here"
-    ),
-    attachments: [
-      {
-        filename: "logo.png",
-        path: __dirname + "/logo.png",
-        cid: "unique@kreata.ee", //same cid value as in the html img src
-      },
-    ],
-  };
-  // console.log("mailOptions : ", mailOptions);
+    var mailOptions = {
+        from: process.env.NODEMAILER_EMAIL,
+        to: `${data.email}`,
+        subject: `DSC - ${data.subject}`,
+        html: sendBody(
+            data.subject,
+            `<p>Hello ${data.name},<br>"Your Issue" - ${data.message} <br /> has been recieved and we will solve this issue soon!!</p>`,
+            "Meanwhile you can explore our website by clicking on the button below",
+            "https://dscnits.tech",
+            "Click Here"
+        ),
+        attachments: [{
+            filename: "logo.png",
+            path: __dirname + "/logo.png",
+            cid: "unique@kreata.ee", //same cid value as in the html img src
+        }, ],
+    };
+    // console.log("mailOptions : ", mailOptions);
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent: " + info.response);
-    }
-  });
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Email sent: " + info.response);
+        }
+    });
 };
 
 //for sending to admin
 const contactAdmin = (data) => {
-  var transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.NODEMAILER_EMAIL, //email id
-      pass: process.env.NODEMAILER_PASSWORD, //my gmail password
-    },
-  });
+    var transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.NODEMAILER_EMAIL, //email id
+            pass: process.env.NODEMAILER_PASSWORD, //my gmail password
+        },
+    });
 
-  // filesArray = data.files.split("--");
+    // filesArray = data.files.split("--");
 
-  let mailContent = `
+    let mailContent = `
   A new Report has been issued.<p>Name - ${data.name}</p><p>Email Id - ${data.email}</p><p>Subject - ${data.subject}</p><p>Message - ${data.message}</p><p>Contains Images:${data.img}`;
-  var mailOptions = {
-    from: process.env.NODEMAILER_SECONDARYEMAIL,
-    to: process.env.NODEMAILER_EMAIL,
-    subject: `DSC User Issue Report`,
-    html: sendBody(
-      `DSC User Issue Report`,
-      mailContent,
-      "Please visit the website to check",
-      "https://dscnits.tech",
-      "Click Here"
-    ),
-    attachments: [
-      {
-        filename: "logo.png",
-        path: __dirname + "/logo.png",
-        cid: "unique@kreata.ee", //same cid value as in the html img src
-      },
-    ],
-  };
-  // console.log("mailOptions : ", mailOptions);
+    var mailOptions = {
+        from: process.env.NODEMAILER_SECONDARYEMAIL,
+        to: process.env.NODEMAILER_EMAIL,
+        subject: `DSC User Issue Report`,
+        html: sendBody(
+            `DSC User Issue Report`,
+            mailContent,
+            "Please visit the website to check",
+            "https://dscnits.tech",
+            "Click Here"
+        ),
+        attachments: [{
+            filename: "logo.png",
+            path: __dirname + "/logo.png",
+            cid: "unique@kreata.ee", //same cid value as in the html img src
+        }, ],
+    };
+    // console.log("mailOptions : ", mailOptions);
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent: " + info.response);
-    }
-  });
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Email sent: " + info.response);
+        }
+    });
 };
 
 //for sending to admin
 const signUpMail = (data, protocol, host) => {
-  const rand = cryptoRandomString({ length: 100, type: "url-safe" });
-  // const host = req.get("host");
-  // const link = `http://localhost:3000/user/verify/${data._id}?tkn=${rand}`;
-  // const link = `https://dscnitsilchar.herokuapp.com/user/verify/${data._id}?tkn=${rand}`;
-  const PORT = process.env.PORT || 8080;
-  const link = `${protocol}://${host}/user/verify/${data._id}?tkn=${rand}`;
-  var transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.NODEMAILER_EMAIL, //email id
-      pass: process.env.NODEMAILER_PASSWORD, //my gmail password
-    },
-  });
-  var mailOptions = {
-    from: process.env.NODEMAILER_EMAIL,
-    to: `${data.email}`,
-    subject: "DSC - Email Verification", 
-    html: sendBody(
-      "Email Verification",
-      ``,
-      `Hello ${data.name}, please Click on the button to verify your email`,
-      link,
-      "Verify Email"
-    ),
-    attachments: [
-      {
-        filename: "logo.png",
-        path: __dirname + "/logo.png",
-        cid: "unique@kreata.ee", //same cid value as in the html img src
-      },
-    ],
-  };
-  // console.log("mailOptions : ", mailOptions);
+    const rand = cryptoRandomString({
+        length: 100,
+        type: "url-safe"
+    });
+    // const host = req.get("host");
+    const link = `http://localhost:8080/user/verify/${data._id}?tkn=${rand}`;
+    // const link = `https://dscnitsilchar.herokuapp.com/user/verify/${data._id}?tkn=${rand}`;
+    const PORT = process.env.PORT || 8080;
+    // const link = `${protocol}://${host}/user/verify/${data._id}?tkn=${rand}`;
+    var transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.NODEMAILER_EMAIL, //email id
+            pass: process.env.NODEMAILER_PASSWORD, //my gmail password
+        },
+    });
+    var mailOptions = {
+        from: process.env.NODEMAILER_EMAIL,
+        to: `${data.email}`,
+        subject: "DSC - Email Verification",
+        html: sendBody(
+            "Email Verification",
+            ``,
+            `Hello ${data.name}, please Click on the button to verify your email`,
+            link,
+            "Verify Email"
+        ),
+        attachments: [{
+            filename: "logo.png",
+            path: __dirname + "/logo.png",
+            cid: "unique@kreata.ee", //same cid value as in the html img src
+        }, ],
+    };
+    // console.log("mailOptions : ", mailOptions);
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log("Error", error);
-    } else {
-      console.log("Email sent: " + info.response);
-    }
-  });
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log("Error", error);
+        } else {
+            console.log("Email sent: " + info.response);
+        }
+    });
 };
 
 // Email to reset password
 const forgotPassword = (data, protocol, host) => {
-  const rand = cryptoRandomString({ length: 100, type: "url-safe" });
-  // const link = `http://localhost:3000/user/verify/forgotpassword/${data._id}?tkn=${rand}`;
-  // const link = `https://dscnitsilchar.herokuapp.com/user/verify/forgotpassword/${data._id}?tkn=${rand}`;
-  const PORT = process.env.PORT || 8080;
-  const link = `${protocol}://${host}/user/verify/forgotpassword/${data._id}?tkn=${rand}`;
+    const rand = cryptoRandomString({
+        length: 100,
+        type: "url-safe"
+    });
+    const link = `http://localhost:8080/user/verify/forgotpassword/${data._id}?tkn=${rand}`;
+    // const link = `https://dscnitsilchar.herokuapp.com/user/verify/forgotpassword/${data._id}?tkn=${rand}`;
+    const PORT = process.env.PORT || 8080;
+    // const link = `${protocol}://${host}/user/verify/forgotpassword/${data._id}?tkn=${rand}`;
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.NODEMAILER_EMAIL, //email id
-      pass: process.env.NODEMAILER_PASSWORD, //my gmail password
-    },
-  });
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.NODEMAILER_EMAIL, //email id
+            pass: process.env.NODEMAILER_PASSWORD, //my gmail password
+        },
+    });
 
-  const mailOptions = {
-    from: process.env.NODEMAILER_EMAIL,
-    to: data.email,
-    subject: "Reset Password",
-    html: sendBody(
-      "Reset Password",
-      ``,
-      "Please Click on the button to reset your password",
-      link,
-      "Reset Password"
-    ),
-    attachments: [
-      {
-        filename: "logo.png",
-        path: __dirname + "/logo.png",
-        cid: "unique@kreata.ee", //same cid value as in the html img src
-      },
-    ],
-  };
-  // console.log(mailOptions);
-  transporter.sendMail(mailOptions, function (error, response) {
-    if (error) {
-      console.log(error);
-      return;
-    } else {
-      console.log("Message sent for forgot Password.");
-    }
-  });
+    const mailOptions = {
+        from: process.env.NODEMAILER_EMAIL,
+        to: data.email,
+        subject: "Reset Password",
+        html: sendBody(
+            "Reset Password",
+            ``,
+            "Please Click on the button to reset your password",
+            link,
+            "Reset Password"
+        ),
+        attachments: [{
+            filename: "logo.png",
+            path: __dirname + "/logo.png",
+            cid: "unique@kreata.ee", //same cid value as in the html img src
+        }, ],
+    };
+    // console.log(mailOptions);
+    transporter.sendMail(mailOptions, function(error, response) {
+        if (error) {
+            console.log(error);
+            return;
+        } else {
+            console.log("Message sent for forgot Password.");
+        }
+    });
 };
 
 const blogReportWarning = (blog, protocol, host) => {
-  const rand = cryptoRandomString({ length: 100, type: "url-safe" });
-  // const link = `http://localhost:3000/user/verify/forgotpassword/${data._id}?tkn=${rand}`;
-  // const link = `https://dscnitsilchar.herokuapp.com/user/verify/forgotpassword/${data._id}?tkn=${rand}`;
-  const PORT = process.env.PORT || 8080;
-  const link = `${protocol}://${host}/blog/view/${blog._id}`;
+    const rand = cryptoRandomString({
+        length: 100,
+        type: "url-safe"
+    });
+    const link = `http://localhost:8080/user/verify/forgotpassword/${data._id}?tkn=${rand}`;
+    // const link = `https://dscnitsilchar.herokuapp.com/user/verify/forgotpassword/${data._id}?tkn=${rand}`;
+    const PORT = process.env.PORT || 8080;
+    // const link = `${protocol}://${host}/blog/view/${blog._id}`;
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.NODEMAILER_EMAIL, //email id
-      pass: process.env.NODEMAILER_PASSWORD, //my gmail password
-    },
-  });
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.NODEMAILER_EMAIL, //email id
+            pass: process.env.NODEMAILER_PASSWORD, //my gmail password
+        },
+    });
 
-  const mailOptions = {
-    from: process.env.NODEMAILER_EMAIL,
-    to: blog.author.email,
-    subject: "Reported Blog Warning",
-    html: sendBody(
-      "Reported Blog Warning",
-      "",
-      `Hello,<br>this is to inform you that one of your blog ${blog.name} has been reported multiple times and maybe deleted if reports continue`,
-      link,
-      "View Blog"
-    ),
-    attachments: [
-      {
-        filename: "logo.png",
-        path: __dirname + "/logo.png",
-        cid: "unique@kreata.ee", //same cid value as in the html img src
-      },
-    ],
-  };
-  // console.log(mailOptions);
-  transporter.sendMail(mailOptions, function (error, response) {
-    if (error) {
-      console.log(error);
-      return;
-    } else {
-      console.log("Message sent for Report Warning.");
-    }
-  });
+    const mailOptions = {
+        from: process.env.NODEMAILER_EMAIL,
+        to: blog.author.email,
+        subject: "Reported Blog Warning",
+        html: sendBody(
+            "Reported Blog Warning",
+            "",
+            `Hello,<br>this is to inform you that one of your blog ${blog.name} has been reported multiple times and maybe deleted if reports continue`,
+            link,
+            "View Blog"
+        ),
+        attachments: [{
+            filename: "logo.png",
+            path: __dirname + "/logo.png",
+            cid: "unique@kreata.ee", //same cid value as in the html img src
+        }, ],
+    };
+    // console.log(mailOptions);
+    transporter.sendMail(mailOptions, function(error, response) {
+        if (error) {
+            console.log(error);
+            return;
+        } else {
+            console.log("Message sent for Report Warning.");
+        }
+    });
 };
 
 //for sending to admin
 const reportBlogToAdmin = (blog, protocol, host) => {
-  var transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.NODEMAILER_EMAIL, //email id
-      pass: process.env.NODEMAILER_PASSWORD, //my gmail password
-    },
-  });
+    var transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.NODEMAILER_EMAIL, //email id
+            pass: process.env.NODEMAILER_PASSWORD, //my gmail password
+        },
+    });
 
-  const link = `${protocol}://${host}/blog/view/${blog._id}`;
+    const link = `${protocol}://${host}/blog/view/${blog._id}`;
 
-  // filesArray = data.files.split("--");
+    // filesArray = data.files.split("--");
 
-  let mailContent = `
+    let mailContent = `
   A new Blog Report has been issued.<p>Name - ${data.name}</p>`;
-  var mailOptions = {
-    from: process.env.NODEMAILER_SECONDARYEMAIL,
-    to: process.env.NODEMAILER_EMAIL,
-    subject: `DSC Blog Issue Report`,
-    html: sendBody(
-      "DSC Blog Issue Report",
-      "",
-      mailContent,
-      link,
-      "Visit Blog"
-    ),
-  };
-  // console.log("mailOptions : ", mailOptions);
+    var mailOptions = {
+        from: process.env.NODEMAILER_SECONDARYEMAIL,
+        to: process.env.NODEMAILER_EMAIL,
+        subject: `DSC Blog Issue Report`,
+        html: sendBody(
+            "DSC Blog Issue Report",
+            "",
+            mailContent,
+            link,
+            "Visit Blog"
+        ),
+    };
+    // console.log("mailOptions : ", mailOptions);
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent: " + info.response);
-    }
-  });
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Email sent: " + info.response);
+        }
+    });
 };
 
 module.exports = {
-  contact,
-  contactAdmin,
-  signUpMail,
-  forgotPassword,
-  reportBlogToAdmin,
-  blogReportWarning,
+    contact,
+    contactAdmin,
+    signUpMail,
+    forgotPassword,
+    reportBlogToAdmin,
+    blogReportWarning,
 };
